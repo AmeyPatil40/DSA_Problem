49ebd18792f409acbaf595226c801be49a1117d4
PatilAmey1 <patilameyshivaji@gmail.com>
2024-08-18 00:27:20 +0530

    added

---
+++ ArrayBasic/function1.js
@@ -0,0 +1,28 @@
+//function statement
+  function a(){
+    console.log("Hello")
+  };
+  a();
+  //function expression
+ 
+  var b=function (){
+    console.log("Hello")
+  }
+  
+
+  ////----------------
+  function getValue(a=10,b=20){
+      a=50;
+      console.log(a);
+  }
+
+  getValue(30,40);
+
+
+
+
+
+
+
+
+
